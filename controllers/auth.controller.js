06db936e69ae7e4525b9dba1551ccfddca34b5c1
vishnuManimala <vishnumanimala8133@gmail.com>
2024-03-20 23:22:42 +0530

    verify otp api created

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -18,20 +18,51 @@ const sendOtp = async (req, res) => {
     const sendMessage = await sharedFunctions.twilio(otp, phoneNumber);
 
     if (sendMessage !== "success") {
-      return res.status(500).json("Something went wrong!");
+      return res.status(500).json({message:"Something went wrong!"});
     }
 
     const isOtpSaved = sharedFunctions.saveOTP(user._id, otp);
 
-    return res.status(200).json("Otp send successfully!");
+    return res.status(200).json({message: "Otp send successfully!"});
   } catch (err) {
-    res.status(500).json("Something went wrong");
+    res.status(500).json({message:"Something went wrong!"});
   }
 };
 
+const verifyOtp = async (req, res) => {
+    const { phoneNumber, otp } = req.body;
+  
+    try {
+  
+      if (!phoneNumber) {
+        return res.status(400).json({ message: "Missing phone number " });
+      }
+  
+      if (!otp) {
+        return res.status(400).json({ message: "Missing  OTP" });
+      }
+  
+      const userOtp = await OtpModel.findOne({phone:phoneNumber, otp:otp});
 
+      if(!userOtp){
+        return res.status(401).json({ message:  'Invalid OTP' });
+      }
+      const currentDate = new Date();
+      if (userOtp.createdAt < currentDate) {
+        return res.status(401).json({ message:  ' OTP expired' });
+      }
+  
+      res.status(200).json({ message: 'OTP verified successfully' });
+      await OtpModel.deleteOne({phone: phoneNumber});
+
+    } catch (err) {
+
+      res.status(500).json({ message: "Something went wrong"});
+    }
+  };
 
 module.exports = {
   sendOtp,
-  
+  verifyOtp,
+  resendOtp
 };
