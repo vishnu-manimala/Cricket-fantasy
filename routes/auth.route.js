8d239abdd8229b96ecdbe60b57bf1d0a1699166c
vishnuManimala <vishnumanimala8133@gmail.com>
2024-03-20 23:42:08 +0530

    auth controller completed

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -4,6 +4,7 @@ const authRoutes = express();
 const authController = require('../controllers/auth.controller');
 
 authRoutes.post('/sendOtp', authController.sendOtp);
-
+authRoutes.post('/verify', authController.verifyOtp);
+authRoutes.post('/reSendOtp', authController.resendOtp);
 
 module.exports = authRoutes;
